a6b8514d5e7250fa16f638c362e4de89e3ddbcc7
Alexander Smirnov <smirnov48@gmail.com>
2018-11-30 19:55:20 +0300

    Добавил звуки

--- www/js/intro.js
+++ www/js/intro.js
@@ -6,18 +6,24 @@ class Intro extends Phaser.Scene {
 
 	preload() {
 		this.load.image("ldlogo", "assets/ldlogo.png");
+		this.load.audio('logohit', ['assets/logohit.wav']);
 	}
 
 	create () {
+		var hitsound = this.sound.add('logohit');
+
 		this.matter.world.setBounds(0, -300, 960, 840);
 		var ldlogo = this.matter.add.image(50, -100, 'ldlogo');
 		ldlogo.setVelocityX(10);
 		ldlogo.setVelocityY(20);
 		ldlogo.setBounce(0.1);
 		ldlogo.setAngularVelocity(Math.random());
+
+  		 this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
+			hitsound.play();
+		});
 	}
 
 	update(time, delta){
 	}
-
 }
