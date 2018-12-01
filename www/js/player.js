3d4ca235204079c5780f05f2ae384c04d9308851
Alexander Smirnov <smirnov48@gmail.com>
2018-12-01 20:10:18 +0300

    теперь кролик перемещается с физикой

--- www/js/player.js
+++ www/js/player.js
@@ -1,20 +1,23 @@
 class Player {
 
 	constructor(game) {
-		this.sprite = game.add.image(70, 50, 'player');
+		this.sprite = game.physics.add.image(70, 50, 'player');
 		this.keys = game.input.keyboard.createCursorKeys();
 	}
 
 	update(time, delta){
 		if (this.keys.left.isDown) {
-			this.sprite.x -= 5;
+			this.sprite.setVelocityX(-50);
 		} else if (this.keys.right.isDown) {
-			this.sprite.x += 5;
+			this.sprite.setVelocityX(50);
 		} else if (this.keys.up.isDown) {
-			this.sprite.y -= 5;
+			this.sprite.setVelocityY(-50);
 		} else if (this.keys.down.isDown) {
-			this.sprite.y += 5;
-		} 
+			this.sprite.setVelocityY(50);
+		} else {
+			this.sprite.setVelocityX(0);
+			this.sprite.setVelocityY(0);
+		}
 	}
 
 }
