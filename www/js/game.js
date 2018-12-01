fb7df00e4c1685e6f27c6f45950cc39f5b7176f0
Alexander Smirnov <smirnov48@gmail.com>
2018-12-02 00:44:49 +0300

    Убрал лишнее

--- www/js/game.js
+++ www/js/game.js
@@ -14,24 +14,24 @@ class Game extends Phaser.Scene {
 
 	create () {
 
-  var wall11 = this.physics.add.staticGroup();
+		var wall11 = this.physics.add.staticGroup();
 
-                  wall11.create(300, 250, 'wall1');
-                wall11.create(600, 250, 'wall1');
-var wall12 = this.physics.add.staticGroup();
+		wall11.create(300, 250, 'wall1');
+		wall11.create(600, 250, 'wall1');
+		var wall12 = this.physics.add.staticGroup();
 
-                wall12.create(400, 250, 'wall2');
-                wall12.create(400, 650, 'wall2');
+		wall12.create(400, 250, 'wall2');
+		wall12.create(400, 650, 'wall2');
 
 		this.add.image(200, 300, 'fon');
 		var npc11 = this.physics.add.sprite(400, 300, 'npc1');
-        npc11.setBounce(0.2);
-	    npc11.setCollideWorldBounds(true);
-        npc11.setVelocityX(50);
-        npc11.setVelocityY(50);
+		npc11.setBounce(0.2);
+		npc11.setCollideWorldBounds(true);
+		npc11.setVelocityX(50);
+		npc11.setVelocityY(50);
 
- this.physics.add.collider(npc11, wall11);
-this.physics.add.collider(npc11, wall12);
+		this.physics.add.collider(npc11, wall11);
+		this.physics.add.collider(npc11, wall12);
 
 		this.cameras.main.setBackgroundColor('rgba(255, 0, 0, 0.5)');
 		this.player = new Player(this);
@@ -40,12 +40,12 @@ this.physics.add.collider(npc11, wall12);
 
 	update(time, delta){
 		this.player.update();
-}
+	}
 
 }
 function hitWalls(npc11, wall11) {
 	if (npc11.body.touching.down) {
-        npc11.setVelocityX(-20);
+		npc11.setVelocityX(-20);
 		npc11.setVelocityY(-20);
 	}
 	if (enemy.body.touching.left) {
@@ -53,12 +53,12 @@ function hitWalls(npc11, wall11) {
 		npc11.setVelocityY(20);
 	}
 
-if (npc11.body.touching.right) {
+	if (npc11.body.touching.right) {
 		npc11.setVelocityX(20);
 		npc11.setVelocityY(-20);
 	}
 	if (npc11.body.touching.up) {
-	    npc11.setVelocityX(20);
+		npc11.setVelocityX(20);
 		npc11.setVelocityY(20);
 	}
 
@@ -67,7 +67,7 @@ if (npc11.body.touching.right) {
 
 function hitWalls2(npc11, wall2) {
 	if (npc11.body.touching.down) {
-        npc11.setVelocityX(-20);
+		npc11.setVelocityX(-20);
 		npc11.setVelocityY(-20);
 	}
 	if (enemy.body.touching.left) {
@@ -75,12 +75,12 @@ function hitWalls2(npc11, wall2) {
 		npc11.setVelocityY(20);
 	}
 
-if (npc11.body.touching.right) {
+	if (npc11.body.touching.right) {
 		npc11.setVelocityX(20);
 		npc11.setVelocityY(-20);
 	}
 	if (npc11.body.touching.up) {
-	    npc11.setVelocityX(20);
+		npc11.setVelocityX(20);
 		npc11.setVelocityY(20);
 	}
 
