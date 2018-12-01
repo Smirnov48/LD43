d767cc76f7176d4d138eaf83f27d740c1866483e
Dronder <and.usp38@gmail.com>
2018-12-01 20:28:52 +0300

    Исправили результат конфликта

--- www/js/game.js
+++ www/js/game.js
@@ -3,24 +3,25 @@ class Game extends Phaser.Scene {
 	constructor() {
 		super( { key: 'Game'});
 	}
-preload () {
-         this.load.image('fon', 'assets/fonTest.png');
-		this.load.image('npc1', 'assets/зшвщк.png');
-        
-}
-	create () {
-		this.add.image(200, 300, 'fon');
-		 npc11 = this.add.sprite(400, 300, 'npc1');
-         npc11.setBounce(0.2);
-	     nps11.setCollideWorldBounds(true);
-         nps11.setVelocityX(Math.random(20));
-         nps11.setVelocityY(Math.random(20));
 
+	preload () {
+		this.load.image('fon', 'assets/fonTest.png');
+		this.load.image('npc1', 'assets/зшвщк.png');
 		this.load.image('player', 'assets/krolik.png');
 	}
 
+	create () {
+		this.add.image(200, 300, 'fon');
+		npc11 = this.physics.add.sprite(400, 300, 'npc1');
+        npc11.setBounce(0.2);
+	    nps11.setCollideWorldBounds(true);
+        nps11.setVelocityX(Math.random(20));
+        nps11.setVelocityY(Math.random(20));
+
 		this.cameras.main.setBackgroundColor('rgba(255, 0, 0, 0.5)');
 		this.player = new Player(this);
+	}
+
 	update(time, delta){
 		this.player.update();
 	}
