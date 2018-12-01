a7de01c48eced54e5c08c5df945959c358f7c273
Alexander Smirnov <smirnov48@gmail.com>
2018-12-01 20:34:19 +0300

    исправил ошибку с npc11

--- www/js/game.js
+++ www/js/game.js
@@ -12,11 +12,11 @@ class Game extends Phaser.Scene {
 
 	create () {
 		this.add.image(200, 300, 'fon');
-		npc11 = this.physics.add.sprite(400, 300, 'npc1');
+		var npc11 = this.physics.add.sprite(400, 300, 'npc1');
         npc11.setBounce(0.2);
-	    nps11.setCollideWorldBounds(true);
-        nps11.setVelocityX(Math.random(20));
-        nps11.setVelocityY(Math.random(20));
+	    npc11.setCollideWorldBounds(true);
+        npc11.setVelocityX(Math.random(20));
+        npc11.setVelocityY(Math.random(20));
 
 		this.cameras.main.setBackgroundColor('rgba(255, 0, 0, 0.5)');
 		this.player = new Player(this);
