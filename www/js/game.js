d04553077b4f699b939a1ecdef29125ff19bfa2d
hl1wd2 <death6412@gmail.com>
2018-12-01 17:45:46 +0300

    Игрок(Не проверенна работа способность!!!)
    Движение игрока, и сама сцена, не проверенно т.к. не понял как работает index.html :\

--- www/js/game.js
+++ www/js/game.js
@@ -5,12 +5,36 @@ class Game extends Phaser.Scene {
 	}
 
 	preload() {
+	this.load.image('background', 'assets/background.png');
+	this.load.image('player', 'assets/krolik.png');
+
 	}
 
 	create () {
+	this.add.image(960, 540, 'background');
+	player = this.physics.add.sprite(70, 50, 'player');
+	player.setBounce(0.2);
+	player.setCollideWorldBounds(true);
+	keys = this.input.keyboard.createCursorKeys();
 	}
 
 	update(time, delta){
+		if (keys.left.isDown) {
+		player.setVelocityX(-160);
+		//player.anims.play('left', true);
+	} else if (keys.right.isDown) {
+		player.setVelocityX(160);
+		//player.anims.play('right', true);
+	} else if (keys.up.isDown) {
+		player.setVelocityY(160);
+		//player.anims.play('right', true);
+	} else if (keys.down.isDown) {
+		player.setVelocityY(-160);
+		//player.anims.play('right', true);
+	} else {
+		player.setVelocityX(0);
+		//player.anims.play('stop');
+	}
 	}
 
 }
