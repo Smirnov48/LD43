de0787e66b96d7b0319be1b3811e58b993cf1093
Alexander Smirnov <smirnov48@gmail.com>
2018-12-02 01:01:29 +0300

    Добавил управление WASD

--- www/js/units/player.js
+++ www/js/units/player.js
@@ -3,6 +3,7 @@ class Player {
 	constructor(game) {
 		this.sprite = game.physics.add.sprite(60, 60, 'player');
 		this.keys = game.input.keyboard.createCursorKeys();
+		this.keysWASD = game.input.keyboard.addKeys('W,A,S,D'); 
 		game.anims.create({
 			key: 'left',
 			frames: game.anims.generateFrameNumbers('player', { start:4, end:7 }),
@@ -37,16 +38,16 @@ class Player {
 	}
 
 	update(time, delta){
-		if (this.keys.left.isDown) {
+		if (this.keys.left.isDown || this.keysWASD.S.isDown) {
 			this.sprite.setVelocityX(-50);
 			this.sprite.anims.play('left', true);
-		} else if (this.keys.right.isDown) {
+		} else if (this.keys.right.isDown || this.keysWASD.D.isDown) {
 			this.sprite.setVelocityX(50);
 			this.sprite.anims.play('right', true);
-		} else if (this.keys.up.isDown) {
+		} else if (this.keys.up.isDown || this.keysWASD.W.isDown) {
 			this.sprite.setVelocityY(-50);
 			this.sprite.anims.play('up', true);
-		} else if (this.keys.down.isDown) {
+		} else if (this.keys.down.isDown || this.keysWASD.A.isDown) {
 			this.sprite.setVelocityY(50);
 			this.sprite.anims.play('down', true);
 		} else {
