135ca412b9ca39310814fd111dfbe1410fdb55cf
Alexander Smirnov <smirnov48@gmail.com>
2018-12-02 03:12:41 +0300

    Сделал карту модельню

--- www/js/map/map.js
+++ www/js/map/map.js
@@ -1,22 +1,17 @@
 class Map {
 
 	constructor(game) {
+		Map.ID_TREE = 0x267F00;
+		Map.ID_GRASS = 0x4CFF00;
+		Map.ID_WATER = 0x0094FF;
+		Map.ID_START_POSITION = 0xFF00FF;
+
+		this.map = new Array();
 		for (var i = 0; i < 150; i++) { 
+			this.map[i] = new Array();
+
 			for (var j = 0; j < 150; j++) { 
-				var color = game.textures.getPixel(i, j, 'map');
-				switch (color._color) {
-					case 0x267F00:  //tree
-						new Tree(game, i * 67, j * 67);
-						break;
-					case 0x4CFF00: //grass
-						new Grass(game, i * 67, j * 67);
-						break;
-					case 0x0094FF://water
-						new Water(game, i * 67, j * 67);
-						break;
-					case 0xFF00FF://start position
-						break;
-				}
+				this.map[i][j] = game.textures.getPixel(i, j, 'map')._color;
 			}
 		}
 	}
