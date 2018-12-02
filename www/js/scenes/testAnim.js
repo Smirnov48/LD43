6c36cbbc92000c25794c6ad3882826e8e4929247
Alexander Smirnov <smirnov48@gmail.com>
2018-12-02 12:14:08 +0300

    добавил тест анимации

--- www/js/scenes/testAnim.js
+++ www/js/scenes/testAnim.js
@@ -11,6 +11,7 @@ class TestAnim extends Phaser.Scene {
 		this.load.image('tree', 'assets/tree.png');
 		this.load.image('water', 'assets/water.png');
 		this.load.image('grass', 'assets/grass.png');
+		this.load.spritesheet('playerRun', 'assets/run.png', { frameWidth: 55, frameHeight: 55 });
 
 		this.keys = this.input.keyboard.addKeys('Y'); 
 	}
@@ -19,15 +20,15 @@ class TestAnim extends Phaser.Scene {
 		this.cameras.main.setBackgroundColor('rgba(0, 255, 0, 0.5)');
 		new Tree(this, 300, 300);
 
-		this.sprite = this.physics.add.sprite(450, 240, 'player');
+		this.sprite = this.physics.add.sprite(450, 240, 'playerRun');
 
 		this.anims.create({
-			key: 'left',
-			frames: this.anims.generateFrameNumbers('player', { start:0, end:3 }),
+			key: 'playerRun',
+			frames: this.anims.generateFrameNumbers('playerRun', { start:0, end:1 }),
 			frameRate: 10,
 			repeat: -1
 		});
-		this.sprite.anims.play('left', true);
+		this.sprite.anims.play('playerRun', true);
 	}
 
 
